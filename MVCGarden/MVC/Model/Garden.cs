4dec5904022aec344b20b29c5e744046248b4ebb
Radost-Kostadinova <Radost-Kostadinova>
2023-02-13 11:06:16 +0200

    add Model

---
+++ MVCGarden/MVC/Model/Garden.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.Model
+{
+    public class Garden
+    {
+        public Garden(decimal priceVegitables, decimal priceFruits, int kgVegitables, int kgFruits)
+        {
+            PriceVegitables = priceVegitables;
+            PriceFruits = priceFruits;
+            KgVegitables = kgVegitables;
+            KgFruits = kgFruits;
+        }
+
+        public decimal PriceVegitables { get; set; }
+        public decimal PriceFruits { get; set; }
+        public int KgVegitables { get; set; }
+        public int KgFruits { get; set; }
+
+        public decimal CalculateSum()
+        {
+            return 1.94m * (this.KgFruits * this.PriceFruits + this.KgVegitables * PriceVegitables);
+
+        }
+
+
+    }
+}
