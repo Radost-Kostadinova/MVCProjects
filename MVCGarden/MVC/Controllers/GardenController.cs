797ac0ad3ae37ccc48e74a994fb642873c782dd0
Radost-Kostadinova <Radost-Kostadinova>
2023-02-13 11:31:30 +0200

    add GardenController

---
+++ MVCGarden/MVC/Controllers/GardenController.cs
@@ -0,0 +1,27 @@
+﻿using MVC.Model;
+using MVC.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.Controllers
+{
+     public class GardenController
+    {
+        private Dispaly display;
+        private Garden garden;
+
+        public GardenController()
+        {
+            display = new Dispaly();
+            garden = new Garden(display.PriceVegitables,display.PriceFruits,display.KgVegitables,display.KgFruits);
+            display.Total = garden.CalculateSum();
+            display.ShowResult();
+        }
+
+
+
+    }
+}
